87bdef21b59eec2926c439cec0bce7aeeeb7a642
vaisse <alistair@vaisse.etu.u-pec.fr>
2023-11-30 00:01:18 +0100

    libreoffice sur la VM pour faire la SAE de DEV

---
+++ tests.c
@@ -0,0 +1,20 @@
+﻿#include <stdlib.h>
+#include <stdio.h>
+#include <graph.h>
+
+int main(void){
+	/*variables locales main*/
+	int run=1;
+	long int clock=0;
+
+	InitialiserGraphique();
+	CreerFenetre(500,500,700,500);
+	head = ChargerSprite(“sprites/head.png”);
+	AfficherSprite(head,100,100);
+	while(run==1){
+		clock=clock+1;	
+	};
+	FermerGraphique();
+
+	return 0;
+}
