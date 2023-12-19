d9c903101f2c2b3a267db3eaa92fd9f8fec1d109
dardouri <louaydardouri16@gmail.com>
2023-12-19 14:11:24 +0100

    Ajout de temps.h

---
+++ main/temps.h
@@ -0,0 +1,8 @@
+#ifndef TEMPS_H
+#define TEMPS_H
+
+void update_timer(unsigned long int start);
+unsigned long int start_timer(unsigned long int start);
+unsigned long int stop_timer(unsigned long int start);
+
+#endif /* TEMPS_H */
