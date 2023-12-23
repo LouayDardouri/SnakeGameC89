67755e3c7b0168c0d8b0d8dd596525b63684aa49
dardouri <louaydardouri16@gmail.com>
2023-12-23 11:39:18 +0100

    realiser par Alistair et louay

---
+++ objet/objets.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/*-----initialisation des objets------*/
+typedef struct {
+	int sprite;
+	unsigned int pos_x;
+	unsigned int pos_y;
+	/*sens_x et sens_y sont dans {-1,0,1}. Ils permettent, selon la valeur, 
+	d'indiquer les dÃ©placements interdits*/
+	int sens_x;
+	int sens_y;
+} bloc;
+
+typedef struct {
+	unsigned short int sprite;
+	unsigned int pos_x;
+	unsigned int pos_y;
+} bloc_static;
